bcd633116a9c22d5329e54b70114ddf77ac6e407
Tyler Lewis <tyhlewis@gmail.com>
2015-05-09 18:10:51 -0700

    first pass at basic computer moving logic to start game; a bunch of helper functions that will be filled out at later times

--- app.js
+++ app.js
@@ -1,36 +1,114 @@
+// 0's for unoccupied spaces, 1's for computer-occupied spaces, 2's for player-occupied spaces
 var game = [
-	[0, 1, 0, 1, 0, 1, 0, 1],
-	[1, 0, 1, 0, 1, 0, 1, 0],
-	[0, 1, 0, 1, 0, 1, 0, 1],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[2, 0, 2, 0, 2, 0, 2, 0],
-	[0, 2, 0, 2, 0, 2, 0, 2],
-	[2, 0, 2, 0, 2, 0, 2, 0]
+	[null, 1, null, 1, null, 1, null, 1],
+	[1, null, 1, null, 1, null, 1, null],
+	[null, 1, null, 1, null, 1, null, 1],
+	[0, null, 0, null, 0, null, 0, null],
+	[null, 0, null, 0, null, 0, null, 0],
+	[2, null, 2, null, 2, null, 2, null],
+	[null, 2, null, 2, null, 2, null, 2],
+	[2, null, 2, null, 2, null, 2, null]
 ];
 
-var computerMove = function() {
+// Store the piece DOM elements in their corresponding space in the game matrix for easy movement manipulation
+var pieces = [ [], [], [], [], [], [], [], [] ];
 
+var computerMove = function() {
+	var jumpsAvailable = checkForJumps(true);
+	if(jumpsAvailable) {
+		// Need to build in ability to move pieces first
+	} else {
+		var availableMove = moveToAvailableSpace(true);
+	}
 };
 
 var playerMove = function() {
+	var jumpsAvailable = checkForJumps(false);
+};
 
+var computerMoveLeft = function(i, j) {
+	return (game[i + 1][j - 1] === 0);
+};
+
+var computerMoveRight = function(i, j) {
+	return (game[i + 1][j + 1] === 0);
+};
+
+var computerJumpLeft = function(i, j) {
+	return (game[i + 1][j - 1] === 2 && game[i + 2][j - 2] === 0);
+};
+
+var computerJumpRight = function(i, j) {
+	return (game[i + 1][j + 1] === 2 && game[i + 2][j + 2] === 0);
+};
+
+var playerMoveLeft = function(i, j) {
+
+};
+
+var playerMoveRight = function(i, j) {
+
+};
+
+var playerJumpLeft = function(i, j) {
+	return;
+};
+
+var playerJumpRight = function(i, j) {
+	return;
 };
 
 var checkForJumps = function(computer) {
 	if(computer) {
-		
+		for(var i = 0; i < game.length; i++) {
+			for(var j = 0; j < game[i].length; j++) {
+				if(game[i][j] === 1) {
+					if(computerJumpLeft(i, j) || computerJumpRight(i, j)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	} else {
+		//
 	}
-}
+};
+
+var moveToAvailableSpace = function(computer) {
+	if(computer) {
+		for(var i = 0; i < game.length; i++) {
+			for(var j = 0; j < game[i].length; j++) {
+				if(game[i][j] === 1) {
+					if(computerMoveLeft(i, j) || computerMoveRight(i, j)) {
+						console.log(pieces[i][j].style)
+						pieces[i][j].style.top = '98px';
+						if(computerMoveLeft(i, j)) {
+							pieces[i][j].style.left = '-68px';
+						} else {
+							pieces[i][j].style.left = '98px';
+						}
+						return true;;
+					}
+				}
+			}
+		}
+		return false;
+	}
+};
+
+var getRandomNumber = function() {
+	return Math.round(Math.random());
+};
 
 var initializeGameboard = function() {
 	var board = document.createElement('div');
 	board.className = 'gameboard';
 	var invert = false;
-	for(var i = 0; i < 8; i++) {
+	for(var i = 0; i < game.length; i++) {
 		var row = document.createElement('div');
 		row.className = 'row';
-		for(var j = 0; j < 8; j++) {
+		for(var j = 0; j < game[i].length; j++) {
 			var space = document.createElement('div');
 			if(invert) {
 				space.className = (j % 2 === 0) ? 'space red' : 'space black';
@@ -45,21 +123,27 @@ var initializeGameboard = function() {
 					} else if(game[i][j] === 2) {
 						piece.className = 'piece white';
 					}
+					pieces[i].push(piece);
 					// piece.addEventListener('mousedown', drag);
 					space.appendChild(piece);
+				} else {
+					pieces[i].push(null);
 				}
+			} else {
+				pieces[i].push(null);
 			}
 			row.appendChild(space);
 		}
 		board.appendChild(row);
 		invert = !invert;
 	}
+	console.log(pieces)
 	document.body.appendChild(board);
 };
 
 var initializeGame = function() {
 	initializeGameboard();
-	var randomFirstMove = Math.round(Math.random());
+	var randomFirstMove = getRandomNumber();
 	var firstMove = (randomFirstMove === 0) ? 'Computer\'s' : 'Your';
 	var turn = document.createElement('h1');
 	turn.className = 'turn';
